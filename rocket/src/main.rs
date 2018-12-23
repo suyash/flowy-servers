165814c03724a53c28ab60431141925ac249996b
Suyash <suyash93@protonmail.com>
2018-12-23 12:20:56 +0530

    rocket: update to rocket 0.4

--- rocket/src/main.rs
+++ rocket/src/main.rs
@@ -1,32 +1,38 @@
-#![feature(plugin)]
-#![plugin(rocket_codegen)]
+#![warn(missing_docs)]
+#![feature(proc_macro_hygiene, decl_macro)]
 
-extern crate redis;
 extern crate rocket;
-
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
+extern crate redis;
+extern crate rocket_contrib;
+extern crate serde_derive;
 
 use std::convert::From;
 use std::io::Cursor;
 
 use redis::{Client, Commands, RedisError};
 
-use rocket::{Outcome, Request, Response, Rocket, State};
+use rocket::{Outcome, Request, Response, Rocket, State, routes, get, post, delete};
 use rocket::fairing::AdHoc;
 use rocket::http::{Header, ContentType, Method, Status};
 use rocket::request::{self, FromRequest};
-use rocket_contrib::{Json, Value};
 
+use rocket_contrib::json;
+use rocket_contrib::json::{Json, JsonValue};
+
+use serde_derive::{Serialize, Deserialize};
+
+
+#[allow(missing_docs)]
 fn main() {
     let client = Client::open("redis://redis:6379").unwrap();
     rocket(client).launch();
 }
 
+/// creates the main Rocket object
 fn rocket(client: Client) -> Rocket {
     rocket::ignite()
         .mount("/", routes![set, get, del])
-        .attach(AdHoc::on_response(|request: &Request, response: &mut Response| {
+        .attach(AdHoc::on_response("cors", |request: &Request, response: &mut Response| {
             // https://github.com/SergioBenitez/Rocket/issues/25#issuecomment-313895086
 
             if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON) {
@@ -45,6 +51,8 @@ fn rocket(client: Client) -> Rocket {
         .manage(client)
 }
 
+/// A single task to be stored.
+#[allow(missing_docs)]
 #[derive(Serialize, Deserialize)]
 struct Task {
     id: String,
@@ -54,8 +62,8 @@ struct Task {
 }
 
 /// set will create or update a new task
-#[post("/set", format = "application/json", data = "<task>")]
-fn set(task: Json<Task>, _key: ApiKey, client: State<Client>) -> Result<Json<Value>, RedisError> {
+#[post("/set", format = "json", data = "<task>")]
+fn set(task: Json<Task>, _key: ApiKey, client: State<Client>) -> Result<JsonValue, RedisError> {
     let conn = client.get_connection()?;
 
     conn.hset(task.0.id.clone(), "text", task.0.text)?;
@@ -67,13 +75,13 @@ fn set(task: Json<Task>, _key: ApiKey, client: State<Client>) -> Result<Json<Val
         conn.lpush(children_id.clone(), child)?;
     }
 
-    Ok(Json(json!({
+    Ok(json!({
         "ok": true,
-    })))
+    }))
 }
 
 /// get will get a task if it exists, or it will 500 out
-#[get("/<id>", format = "application/json")]
+#[get("/<id>", format = "json")]
 fn get(id: String, _key: ApiKey, client: State<Client>) -> Result<Json<Task>, RedisError> {
     let conn = client.get_connection()?;
 
@@ -88,16 +96,17 @@ fn get(id: String, _key: ApiKey, client: State<Client>) -> Result<Json<Task>, Re
 }
 
 /// delete will remove a task
-#[delete("/<id>", format = "application/json")]
-fn del(id: String, _key: ApiKey, client: State<Client>) -> Result<Json<Value>, RedisError> {
+#[delete("/<id>", format = "json")]
+fn del(id: String, _key: ApiKey, client: State<Client>) -> Result<JsonValue, RedisError> {
     let conn = client.get_connection()?;
     conn.del(id)?;
 
-    Ok(Json(json!({
+    Ok(json!({
         "ok": true,
-    })))
+    }))
 }
 
+/// API_KEY from the environment, stored at compile time.
 const API_KEY: &'static str = env!("API_KEY");
 
 /// https://api.rocket.rs/rocket/request/trait.FromRequest.html#example-1
@@ -126,14 +135,16 @@ mod test {
     extern crate serde_json;
 
     use rocket::local::Client;
-    use rocket::http::{ContentType, Status};
+    use rocket::http::{ContentType, Status, Header};
 
-    use super::{rocket, Task};
+    use super::{rocket, Task, API_KEY};
 
     #[test]
     fn works() {
         let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
         let client = Client::new(rocket(client)).unwrap();
+
+        let api_key_header = Header::new("X-API-Key", API_KEY);
 
         let task = Task{
             id: "test".to_owned(),
@@ -145,6 +156,7 @@ mod test {
         let res = client
             .post("/set")
             .header(ContentType::JSON)
+            .header(api_key_header.clone())
             .body(serde_json::to_string(&task).unwrap())
             .dispatch();
 
@@ -153,6 +165,7 @@ mod test {
         let mut res = client
             .get(format!("/{}", task.id))
             .header(ContentType::JSON)
+            .header(api_key_header.clone())
             .dispatch();
 
         assert_eq!(res.status(), Status::Ok);
@@ -168,6 +181,7 @@ mod test {
         let res = client
             .delete(format!("/{}", task.id))
             .header(ContentType::JSON)
+            .header(api_key_header)
             .dispatch();
 
         assert_eq!(res.status(), Status::Ok);
